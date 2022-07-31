2feadae8712f0d49e17b74d48cf0f666d7d6317c
Ola <train1@tharwat.loc>
2022-07-31 11:51:08 +0300

    Add Repository for product and category

---
+++ Shopping CartProject/ShoppingCart.DataAccess/Repository/CategoriesRepository.cs
@@ -0,0 +1,53 @@
+﻿using ShoppingCart.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Repository
+{
+    public class CategoriesRepository : IShoppingCartRepository<ProductsCategories>
+    {
+        ShoppingCartDbContext db;
+        public CategoriesRepository(ShoppingCartDbContext _db)
+        {
+            db = _db;
+        }
+
+        public void Add(ProductsCategories entity)
+        {
+            db.productsCategories.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var category = Find(id);
+            db.Remove(category);
+            db.SaveChanges();
+        }
+
+
+
+        public ProductsCategories Find(int id)
+        {
+            var category = db.productsCategories.SingleOrDefault(c => c.Id == id);
+            return category;
+        }
+
+        public IList<ProductsCategories> List()
+        {
+           return db.productsCategories.ToList();
+        }
+
+
+
+        public void Update(int id, ProductsCategories NewCategories)
+        {
+            db.Update(NewCategories);
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ Shopping CartProject/ShoppingCart.DataAccess/Repository/ProductsRepository.cs
@@ -0,0 +1,49 @@
+﻿using ShoppingCart.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Repository
+{
+    public class ProductsRepository : IShoppingCartRepository<Products>
+    {
+        ShoppingCartDbContext db;
+
+        public ProductsRepository(ShoppingCartDbContext _db)
+        {
+            this.db = _db;
+        }
+
+        public void Add(Products entity)
+        {
+            db.products.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var products = db.Find(id);
+            db.products.Remove(products);
+            db.SaveChanges();
+        }
+
+        public Products Find(int id)
+        {
+           var product = db.products.SingleOrDefault(p => p.Id == id);
+            return product;
+        }
+
+        public IList<Products> List()
+        {
+            return db.products.ToList();
+        }
+
+        public void Update(int id, Products NewProduct)
+        {
+            db.Update(NewProduct);
+            db.SaveChanges();
+        }
+    }
+}
