df4431b91aad94fdacecbb7c4fc178765922e6ed
Ola <train1@tharwat.loc>
2022-07-31 10:55:50 +0300

    create new project with db context

---
+++ Shopping CartProject/ShoppingCart.DataAccess/ShoppingCartDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess
+{
+    public class ShoppingCartDbContext : IdentityDbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions<ShoppingCartDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Products> products { get; set; }
+        public DbSet<ProductsCategories> productsCategories { get; set; }
+
+    }
+}
